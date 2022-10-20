1e8dcb0d3cdfc8082c84ae067de9d26e2917acf1
BeyondJacob <owensinnovation@gmail.com>
2022-10-20 04:28:54 -0400

    Info Dialog update
    + Placed info dialog box
    = With universal pop-up class/ID for other button dialog access in the future.

---
+++ scripts/menu.js
@@ -0,0 +1,7 @@
+function openForm() {
+  document.getElementById("infoButton").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("infoButton").style.display = "none";
+}
